f3ac6a6f8f117bc3cf895fa72df2261108328d16
xdx1354 <stanislaw1354@gmail.com>
2023-04-12 19:26:00 +0200

    Channel reworked

--- channel.py
+++ channel.py
@@ -3,50 +3,61 @@ import random
 
 class channel:
     def __init__(self, bits_list):
-        self.bits = bits_list
+        self.input_bits = bits_list.copy()
+        self.output_bits = bits_list.copy()
+        self.error_ratio = 0
+        self.errors = 0
+        self.length = len(bits_list)
 
-    def random_error(self, amount_of_errors):
-        for i in range(0, amount_of_errors):  # generating x mistakes in random places
-            rand_location = random.randint(0, len(self.bits) - 1)
-            self.bits[rand_location] = (self.bits[rand_location] + 1) % 2
-        return self.bits
+    def random_errors(self, error_ratio):
+
+        for i in range(0, int(self.length * error_ratio)):  # generating x mistakes in random places
+            rand_location = random.randint(0, self.length - 1)
+            # print(rand_location, "\n")
+            self.output_bits[rand_location] = (self.output_bits[rand_location] + 1) % 2  # zamiana na przeciwny bit
+
+        self.errors = int(self.length * error_ratio)
+        self.error_ratio = error_ratio
 
     def group_errors(self, size_of_group, space_between_groups):
         i = 0
-        while (i + size_of_group) < (len(self.bits)):
+        temp_errors = 0
+        while (i + size_of_group) < self.length:
 
             for j in range(0, size_of_group):
-                # print(i + j, '\n')
-                self.bits[i + j] = (self.bits[i + j] + 1) % 2  # changing to opposite value
+                self.output_bits[i + j] = (self.output_bits[i + j] + 1) % 2  # changing to opposite value
                 j += j
+                temp_errors = temp_errors + 1
 
-            i = i + j + space_between_groups  # skipping next x bits
+            i = i + j + space_between_groups - 1  # skipping next x bits
             j = 0
-            print(i, '\n')
-        return self.bits
+        self.errors = temp_errors
+        self.error_ratio = temp_errors / self.length  # TODO: zaokraglanie do ?
 
     def group_noise(self, size_of_group, space_between_groups):
         i = 0
-        while (i + size_of_group) < (len(self.bits)):
+        temp_errors = 0
+        while (i + size_of_group) < self.length:
             noise_value = random.randint(0, 1)  # picking randomly whether the group noise will be all zeros or all ones
             for j in range(0, size_of_group):
-                # print(i + j, '\n')
-                self.bits[i + j] = noise_value
+                self.output_bits[i + j] = noise_value
                 j += j
+                temp_errors = temp_errors + 1
 
-            i = i + j + space_between_groups  # skipping next x bits
+            i = i + j + space_between_groups - 1  # skipping next x bits
             j = 0
-            print(i, '\n')
-        return self.bits
+            self.errors = temp_errors
+            self.error_ratio = temp_errors / self.length  # TODO: zaokraglanie do ?
 
     # p1 - probability of the channel being in the good state
     # p2 - probability of error in the bad or transition state
     # pg - probability of error in the good state
     # pb - probability of error in the bad or transition state
+
     def gilbert_elliot_model(self, p1, p2, pg, pb):
         state = 1  # initialize to good state
         received_bits = []
-        for bit in self.bits:
+        for bit in self.output_bits:
             # determine the probability of staying in the current state or transitioning to the other state
             if state == 1:  # good state
                 p = p1
@@ -71,4 +82,4 @@ class channel:
             else:  # stay in current state and no error
                 received_bits.append(bit)
 
-        return received_bits
+        self.output_bits = received_bits
