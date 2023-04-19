00b919f82946246dd2a852f589776d0cd32089b8
Anatorini <marcin.szymczak156@gmail.com>
2023-04-19 21:20:47 +0200

    Actually added the decoder (this one works). Also unified return types of encoder/decoder and channel

--- script.py
+++ script.py
@@ -8,18 +8,11 @@ import os
 
 encoder = en.encoder()
 decoder = de.decoder()
-channel = ch.channel()
-#code = km.HammingCode(3)
-# os.system("cls")
-#numbers = [x for x in range(10)]
-#payload = ut.decToBinaryList(numbers)
-#payload = [int(x) for x in payload]
-#codeWords = encoder.encode(payload, encoder.HAMMING, 3)
-#codeWords = [ut.swapEncoding(x, "data_parity") for x in codeWords]
-#data = [code.decode(x) for x in codeWords]
-#data = ut.unPartition(data)
-#data = ut.partition(data, 8)
 
-#data = ut.binaryListToDec(data)
-# for d in data:
-#    print(d, end="\n")
+
+data = [1, 1, 1, 1, 1, 1]
+print(data)
+codeWords = encoder.encode(data, encoder.REPEAT, 3)
+decodedData = decoder.decode(codeWords, decoder.REPEAT, 3)
+
+print(decodedData)
