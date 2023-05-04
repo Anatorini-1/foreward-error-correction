3a936745562a406cc97e124cb18c087921355dd2
xdx1354 <stanislaw1354@gmail.com>
2023-05-04 23:02:52 +0200

    drafts of several test functions added

--- tests.py
+++ tests.py
@@ -5,7 +5,14 @@ from util import Util as ut
 import matplotlib.pyplot as plt
 from PIL import Image
 
+repeatEncodingParams = [1, 2, 3, 4, 5, 6]
+blockEncodingParams = [1, 2, 3, 4, 5, 6]
+hammingEncodingParams = [3, 4, 5, 6]
+bchEncodingParams = [3, 4, 5, 6]
+reedSolomonEncodingParams = [2, 3, 4, 5, 6]  # TODO: sprawdzic jakie parametry powinny byc testowane
 
+
+# nie bedzie dzialac dla hamminga bo dane input są dłuższe niż dane output przez dodanie zer
 def calculateErrors(input: list, output: list) -> int:
     errors = 0
     for i in range(len(input)):
@@ -14,17 +21,28 @@ def calculateErrors(input: list, output: list) -> int:
     return errors
 
 
-def test_repeat():
-    data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
+# TODO: switch case dla różnych modeli
+# models parameters:
+# 1 = random errors
+# 2 = group noises
+# 3 = gilbert_eliot_model
 
-    enValues = [2, 3, 4, 5, 6]
+def test_repeat(model, encodingParams):
+    data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
     x = []  # redundancja
     y = []  # błedy na koniec / pierwotna dlugosc wiadomosci
     en = encoder()
-    for val in enValues:
+    for val in encodingParams:
         encodedData = en.encode(data, 3, val)
+
         ch = channel(encodedData)
-        ch.random_errors(0.2)
+        if model == 0:
+            ch.random_errors(0.4)
+        elif model == 1:
+            ch.group_noise(2, 10)
+        elif model == 2:
+            pass  # ch.gilbert_elliot_model(0.1, )
+
         dec = decoder()
         decodedData = dec.decode(ch.output_bits, 3, val)
         x.append(len(encodedData) / len(data))  # dopisuje obecna redundancje
@@ -34,25 +52,124 @@ def test_repeat():
     # resultImage = ut.creteImageFromBinary(decodedData, 100, 100)
     # resultImage.show()
 
-
-def test_hamming():
+# block code nie jest jeszcze napisany
+def test_block(model, encodingParams):
     data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
-
-    enValues = [3]
     x = []  # redundancja
     y = []  # błedy na koniec / pierwotna dlugosc wiadomosci
     en = encoder()
-    for val in enValues:
-        encodedData = en.encode(data, 0, val)
+    for val in encodingParams:
+        encodedData = en.encode(data, 2, val)
+
         ch = channel(encodedData)
-        ch.random_errors(0.2)
+        if model == 0:
+            ch.random_errors(0.4)
+        elif model == 1:
+            ch.group_noise(2, 10)
+        elif model == 2:
+            pass  # ch.gilbert_elliot_model(0.1, )
+
         dec = decoder()
-        decodedData = dec.decode(ch.output_bits, 0, 3)
+        decodedData = dec.decode(ch.output_bits, 2, val)
         x.append(len(encodedData) / len(data))  # dopisuje obecna redundancje
         y.append(float(calculateErrors(data, decodedData) / len(data)))
     plt.plot(x, y)
     plt.show()
+    # resultImage = ut.creteImageFromBinary(decodedData, 100, 100)
+    # resultImage.show()    #
+
+def test_hamming(channelModel, encodingParams):
+    data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
+    cpyData = data.copy()
+
+    x = []  # redundancja
+    y = []  # błedy na koniec / pierwotna dlugosc wiadomosci
+    en = encoder()
+    for val in encodingParams:
+        print('redundancy: ', val)
+        print('data',
+              len(data))  # TODO: kodowanie wiadomosci wydluza ją zapełaniając zerami brakujące miejsca, ale dlaczego dla redundancji 4 giną 2 bity
+        encodedData = en.encode(data, 0, val)
+        print('encodedData', len(encodedData))
+
+        ch = channel(encodedData)
+        if channelModel == 0:
+            ch.random_errors(0.2)
+        elif channelModel == 1:
+            ch.group_noise(2, 8)
+        elif channelModel == 2:
+            pass  # ch.gilbert_elliot_model(0.1, )
+
+        dec = decoder()
+        decodedData = dec.decode(ch.output_bits, 0, val)
+        x.append(len(encodedData) / len(data))  # dopisuje obecna redundancje
+
+        print('encoding reverted', (len(encodedData) / (2 ** val - 1)) * (2 ** val - 1 - val))
+        print('decoded', len(decodedData))
+        print('\n')
+        # y.append(float(calculateErrors(data, decodedData) / len(data)))
+    # plt.plot(x, y)
+    # plt.show()
+
+def test_bch(channelModel, encodingParams):
+    data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
+    cpyData = data.copy()
+
+    x = []  # redundancja
+    y = []  # błedy na koniec / pierwotna dlugosc wiadomosci
+    en = encoder()
+    for val in encodingParams:
+        print('data', len(data))
+        encodedData = en.encode(data, 5, val)
+
+        ch = channel(encodedData)
+        if channelModel == 0:
+            ch.random_errors(0.2)
+        elif channelModel == 1:
+            ch.group_noise(2, 8)
+        elif channelModel == 2:
+            pass  # ch.gilbert_elliot_model(0.1, )
+
+        dec = decoder()
+        decodedData = dec.decode(ch.output_bits, 5, val)
+        x.append(len(encodedData) / len(data))  # dopisuje obecna redundancje
+
+        print('decoded', len(decodedData))
+        print('\n')
+        # y.append(float(calculateErrors(data, decodedData) / len(data)))
+    # plt.plot(x, y)
+    # plt.show()
 
 
-#test_repeat()
-test_hamming()
+def test_reed_solomon(channelModel, encodingParams):
+    data = ut.readImageToBinary(R"C:\Users\stani\Desktop\proba.png")
+
+    x = []  # redundancja
+    y = []  # błedy na koniec / pierwotna dlugosc wiadomosci
+    en = encoder()
+    for val in encodingParams:
+        print('data', len(data))
+        encodedData = en.encode(data, 6, val)
+
+        ch = channel(encodedData)
+        if channelModel == 0:
+            ch.random_errors(0.2)
+        elif channelModel == 1:
+            ch.group_noise(2, 8)
+        elif channelModel == 2:
+            pass  # ch.gilbert_elliot_model(0.1, )
+
+        dec = decoder()
+        decodedData = dec.decode(ch.output_bits, 6, val)
+        x.append(len(encodedData) / len(data))  # dopisuje obecna redundancje
+
+        print('decoded', len(decodedData))
+        print('\n')
+        # y.append(float(calculateErrors(data, decodedData) / len(data)))
+    # plt.plot(x, y)
+    # plt.show()
+
+
+# test_repeat(1, repeatEncodingParams)
+test_block(1, blockEncodingParams)
+# test_hamming(1, hammingEncodingParams)
